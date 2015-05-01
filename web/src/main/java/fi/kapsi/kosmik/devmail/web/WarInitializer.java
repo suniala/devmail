3445b5cd025ebe8bace335cd517d8f96a5c100fd
Mikko Suniala <mikko.suniala@iki.fi>
2015-05-01 17:09:00 +0300

    Reformat code.

--- web/src/main/java/fi/kapsi/kosmik/devmail/web/WarInitializer.java
+++ web/src/main/java/fi/kapsi/kosmik/devmail/web/WarInitializer.java
@@ -4,8 +4,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
- * This class is needed for deployment on an application server.
- * It is the counterpart of the main method in WicketWebApplication.
+ * This class is needed for deployment on an application server. It is the
+ * counterpart of the main method in WicketWebApplication.
  */
 public class WarInitializer extends SpringBootServletInitializer {
     @Override
